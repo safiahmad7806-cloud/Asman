a7c6452aba794b32542799c116d2fefeb6da10f5
Ahmad <safiahmad7806@gmail.com>
2026-08-08 12:06:30 +0530

    Use environment variables for bot token and chat ID
    Updated bot token and target chat ID to use environment variables.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
 # Read from environment variables (set these in your shell/host, not in code)
-BOT_TOKEN = 8501592088:AAEdyVB18kZLgzAfklsdDk0lvxj-uxHcmaQ
-TARGET_CHAT_ID = -1002912706519
+BOT_TOKEN = os.getenv(8501592088:AAHODptUi4BMNIf59nVfmA8aw9lilwN0-IM)
+TARGET_CHAT_ID = int(os.getenv(-1002912706519))
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
